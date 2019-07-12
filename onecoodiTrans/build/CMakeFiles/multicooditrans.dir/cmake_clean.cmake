dfd32c0d755ead1bcf4d72dbb90aedb17b494992
dmasatoumae <masatodomae@gmail.com>
2019-07-12 23:24:35 +0900

    aaa

---
+++ onecoodiTrans/build/CMakeFiles/multicooditrans.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multicooditrans.dir/main.cpp.o"
+  "multicooditrans.pdb"
+  "multicooditrans"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multicooditrans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
